2c435f0550a76dc5fa2502c4eba81376fe95ee8a
latarniakonto <wisniewskikq27@gmail.com>
2022-01-25 22:23:19 +0100

    Rendered expresssion in game window

--- App/expression_game.py
+++ App/expression_game.py
@@ -1,13 +1,38 @@
-
 # Import and initialize the pygame library
 import pygame
+from expression import (
+    Constant, Variable,
+    Add, Subtract, Times, Divide,
+    Instruction, Assign, While, If
+)
+from expression_formatter import decode_expression
+from expression_data_base import (
+    init,
+    ArithmeticExpression,
+    ProgrammingExpression,        
+)
 
+def get_expressions_from_data_base(session):    
+    expressions = []
+
+    for row in session.query(ArithmeticExpression):
+        e = decode_expression(row.expression_encoding)        
+        expressions.append(e)
+    for row in session.query(ProgrammingExpression):        
+        e = decode_expression(row.expression_encoding)        
+        expressions.append(e)
+
+    return expressions
+
+
+database_session = init()
 pygame.init()
 # Set up the drawing window
 screen = pygame.display.set_mode([1000, 1000])
 # Run until the user asks to quit
 running = True
 font = pygame.font.Font("./Font/expression_font.ttf", 12)
+expressions = get_expressions_from_data_base(database_session)
 
 while running:
 
@@ -20,9 +45,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-
-    text = font.render("while (x!=3)", True, (0, 255, 0))
-    screen.blit(text, (300, 400))
+        
+    text = font.render(str(expressions[1]), True, (0, 255, 0))
+    screen.blit(text, (30, 40))
     # Flip the display
     pygame.display.flip()
 
