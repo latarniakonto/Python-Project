109fe18eb465167711b892a01f0098b3197f1388
latarniakonto <wisniewskikq27@gmail.com>
2022-01-26 11:43:15 +0100

    Created print function that supports newlines

--- App/expression_game.py
+++ App/expression_game.py
@@ -24,6 +24,31 @@ def get_expressions_from_data_base(session):
 
     return expressions
 
+def print_expressions():
+    pos_x = 30
+    pos_y = 40          
+
+    e_iter = 0    
+    for e in expressions: 
+        e_text = str(e)        
+        lines = []        
+        line = ""
+        # pygame doesn't support newlines :(, so here is solution for that
+        for char in e_text:            
+            if char == '\n':                
+                lines.append(font.render(line, True, (0, 255, 0)))
+                line = ""
+            else:
+                line += char
+        if line != "":
+            lines.append(font.render(line, True, (0, 255, 0)))                        
+        
+        for line in range(len(lines)):            
+            screen.blit(lines[line],(pos_x, pos_y + ((e_iter + line) * 2 * font_size)))
+        e_iter = len(lines) + 1
+
+        
+
 
 database_session = init()
 pygame.init()
@@ -31,7 +56,8 @@ pygame.init()
 screen = pygame.display.set_mode([1000, 1000])
 # Run until the user asks to quit
 running = True
-font = pygame.font.Font("./Font/expression_font.ttf", 12)
+font_size = 12
+font = pygame.font.Font("./Font/expression_font.ttf", font_size)
 expressions = get_expressions_from_data_base(database_session)
 
 while running:
@@ -45,9 +71,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        
-    text = font.render(str(expressions[1]), True, (0, 255, 0))
-    screen.blit(text, (30, 40))
+    print_expressions()
     # Flip the display
     pygame.display.flip()
 
