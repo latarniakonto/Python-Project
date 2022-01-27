deef00f8dffc6e192fe5a987388cc89bf7ccef09
latarniakonto <wisniewskikq27@gmail.com>
2022-01-27 15:21:48 +0100

    Created button class

---
+++ App/expression_game_button.py
@@ -0,0 +1,22 @@
+
+
+class Button():
+    def __init__(self, image, x_coord, y_coord, text, font):
+        self.image = image
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.image_rect = self.image.get_rect(center=(self.x_coord, 
+                                                      self.y_coord))
+        self.text = text
+        self.font = font
+        self.display_text = self.font.render(self.text, True, (0, 255, 0))
+        self.text_rect = self.display_text.get_rect(center=(self.x_coord, 
+                                                            self.y_coord))
+    def update(self, screen):
+        screen.blit(self.image, self.image_rect)
+        screen.blit(self.display_text, self.text_rect)
+
+    def is_clicked(self, position):
+        return (position[0] in range(self.rect.left, self.rect.right) and
+                position[1] in range(self.rect.top, self.rect.bottom))
+
