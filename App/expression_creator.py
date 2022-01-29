3ce7ad1f45cac53c813201b54fdd7ab36d1a78a2
latarniakonto <wisniewskikq27@gmail.com>
2022-01-29 12:05:38 +0100

    Added 2 new expressions

--- App/expression_creator.py
+++ App/expression_creator.py
@@ -17,6 +17,21 @@ from expression_data_base import (
 session = init()
 
 if __name__ == "__main__":
+
+    exp1 = Divide(Times(Subtract(Variable("x"), Constant(1)),
+                    Variable("x")), 
+                Constant(2))
+    exp1_code = encode_expression(exp1)
+    print(exp1)
+    session = add_arithmetic_expression(exp1_code, session)
+
+    exp2 = Add(Divide(Times(Subtract(Constant(0), Constant(22)),
+                    Constant(3)), Constant(11)),
+                Constant(17))
+    exp2_code = encode_expression(exp2)
+    print(exp2)
+    session = add_arithmetic_expression(exp2_code, session)
+
     inst = Instruction(
         Assign(Variable("x"), Constant(-10)), 
         While(
@@ -25,13 +40,6 @@ if __name__ == "__main__":
     inst_code = encode_expression(inst)
     session = add_programming_expression(inst_code, session)
     print(inst)
-
-    exp1 = Divide(Times(Subtract(Variable("x"), Constant(1)),
-                    Variable("x")), 
-                Constant(2))
-    exp1_code = encode_expression(exp1)
-    print(exp1)
-    session = add_arithmetic_expression(exp1_code, session)
 
     inst1 = Instruction(Assign(Variable("x"), Constant(-4)), 
                     If(Variable("x"), 
@@ -44,5 +52,18 @@ if __name__ == "__main__":
     print(inst1)
     inst_code1 = encode_expression(inst1)
     session = add_programming_expression(inst_code1, session)
+
+    inst2 = Instruction(
+        Assign(Variable("y"), Constant(-10)),
+        Instruction(Assign(Variable("x"), Constant(2)),
+        While(
+            Variable("y"),
+            Instruction(Assign(Variable("y"), Add(Variable("y"), Constant(1))), 
+            Assign(Variable("x"), Times(Variable("x"), Constant(3))))
+            )))
+    inst_code2 = encode_expression(inst2)
+    session = add_programming_expression(inst_code2, session)
+    print(inst2)
+
     session.commit()
     session.close() 
